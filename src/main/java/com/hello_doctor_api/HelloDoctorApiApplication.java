2d20a47f4b1c14f82d73e28bc7c2822de37b70a5
Palash Trivedi <thinkert26@gmail.com>
2025-01-22 11:03:53 +0530

    basic_structure
    basic_structure

---
+++ src/main/java/com/hello_doctor_api/HelloDoctorApiApplication.java
@@ -0,0 +1,13 @@
+package com.hello_doctor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDoctorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDoctorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello_doctor_api/HelloDoctorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello_doctor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDoctorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
